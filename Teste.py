bb9233c148d12139023c9d8692be86830d0a8762
Felipe Dias <15860964+FelipeDiasb@users.noreply.github.com>
2025-02-27 21:54:41 -0300

    Add files via upload

---
+++ Teste.py
@@ -0,0 +1,105 @@
+ #comentario
+ # Para carregar o código -> F5
+
+ # Dados do tipo texto
+
+#import pyautgui
+
+#Verificar qual o tipo de dado
+#type ()
+
+# Serve para mostrar algo no ambiente de testes
+# Função  - ação
+#  Trabalhando com ( Print)
+#print()
+# dados do tipo texto
+#print(type('Boa noite como vai'))
+# dados do  tipo inteiro
+#print(type(10))
+# dados do tipo Real
+#print(type(10.5))
+# dados do tipo Real
+#print(type(True))
+# O print é uma função de saida
+#print(10+20)
+
+#n = 10
+#print(n)
+# input vai inserir um dado
+#print (nome)
+#idade = input ('Didite sua idade:')
+#print(idade,'Ano:')
+#print(nome,'Anos')
+
+
+
+# entrada
+n1  = float(input(':'))
+n2  = float(input(':'))
+
+# processamento
+soma =  n1 + n2
+sub = n1 - n2
+mult =  n1 * n2
+div = n2/n2
+
+# saída
+#print(f '''
+
+#Soma - {soma}
+#Subtração - {sub}
+#Multiplicação - {mult}
+#Divisão -  {div}
+
+#''')
+
+
+#---------------------
+
+
+#nota_1 = float(input('Nota 1 = '))
+#nota_2 = float(input('Nota 2 = '))
+#nota_3 = float(input('Nota 3 = '))
+#nota_4 = float(input('Nota 4 = '))
+#nota_5 = float(input('Nota 5 = '))
+
+#media  =  nota_1 + nota_2 + nota_3 + nota_4 + nota_2
+#media =  media /5
+#print('Média do aluno',nome, ':', media )
+
+#aprovado = media>=7
+#recuperacao = media>=5 and media <=6.9
+#reprovado  =  media <5
+
+#print('Situação: Aprovado: ', aprovado)
+#print('Situação: Recuperação', recuperacao)
+#print('Situação: Reprovado ', reprovado)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
