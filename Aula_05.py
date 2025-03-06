d26649e06e45739bd1d5eda2f1da77fe2b7d42e9
Felipe Dias <15860964+FelipeDiasb@users.noreply.github.com>
2025-03-05 21:55:24 -0300

    Add files via upload

---
+++ Aula_05.py
@@ -0,0 +1,130 @@
+
+
+# nome= (type(input('Digite o nome: ')))
+# cumprimento = (type(input("Digite uma cumprimento:")))
+# senha = (type (input('Digite sua senha:')))
+
+
+
+
+
+# print(cumprimento, nome,'Sua senha é',senha)
+# print(f'{cumprimento} {nome} Sua senha é {senha}')
+# print(cumprimento + " "+ nome+ " " + 'Sua senha é',senha )
+# print(cumprimento, nome, 'Sua senha é {}'.format(senha))
+# print(cumprimento, nome, 'Sua senha é %s'%(senha))
+
+
+
+
+
+# # CONCATENAR = JUNTAR DADOS 
+# nome  =  'Pedro'
+# # virgula 
+# print('Olá',nome)
+
+
+# # sinal de mais (precisam ser o mesmo tipo de dado)
+
+
+# print('olá'+ '' + nome)
+
+
+# # função format 
+
+
+# print('olá {}'.format(nome))
+
+
+
+# # % com a porcentagem
+
+
+# print('olá %s '%(nome))
+
+
+
+# # f string
+
+
+
+# print(f'olá {nome}')
+
+
+
+
+# texto = "Python" 
+# comprimento = len(texto)  # Isso irá retornar 6 as quantidades de letras que está no str na variavel Texto: 6 letras 
+# print(comprimento)
+
+# 1. Mensagem de boas-vindas
+#print("Bem-vindo ao sistema de operações matemáticas e cadastro!")
+
+# 2.  Declare uma variável booleana verdadeiro Declare uma variável booleana verdadeiro com o valor True e imprima seu tipo
+verdadeiro = bool(input('numero:'))
+print("O tipo da variável 'verdadeiro' é:", (verdadeiro))
+
+# 3. Imprima o resultado da multiplicação de dois números decimais de sua escolha
+decimal1 = 5.2
+decimal2 = 3.8
+print(f"Multiplicação de dois números decimais:", (decimal1) * (decimal2))  
+
+#  4. Imprima o resultado da divisão de dois números inteiros
+inteiro1 = 10
+inteiro2 = 2
+print("Divisão de dois números inteiros:", inteiro1 / inteiro2)
+
+# # 5. Imprima o resultado da subtração de dois números inteiros de sua escolha
+print("Subtração de dois números inteiros:", inteiro1 - inteiro2)
+
+# 6. Imprima o resultado da divisão inteira de dois números inteiros 
+print("Divisão inteira de dois números inteiros:", inteiro1 // inteiro2)
+
+# # # 7. Imprima o resultado da multiplicação de 4 números decimais de sua escolha
+decimal3 = 1.5 
+decimal4 = 2.0 
+decimal5 = 0.5
+decimal6 = 4.0
+
+print("Multiplicação de quatro números decimais:", decimal3 * decimal4 * decimal5 * decimal6)
+
+#  8. Declare uma variável numero e atribua um número inteiro. Em seguida, imprima o dobro desse número
+numero = 7
+dobro = numero * 2
+print(f"O dobro do número é:", {dobro})
+print('''
+      
+************* Calculadora ******************************************
+
+      ''')
+# # 9. Crie uma calculadora de soma com as ferramentas que vc já conhece(Use apenas input, print e variavel)
+
+range(
+ 
+num1 = int(input("Digite o primeiro número: ")) in range(1,4)
+num2 = int(input("Digite o segundo número: "))
+)
+print("Resultando:", num1 + num2)
+print('''
+      
+************************* Cadastro ******************************************
+      ''')
+# 10. Crie um sistema de cadastro com as estruturas que vc já conhece(Use apenas input, print e variavel)
+
+nome = str(input("Digite seu nome: "))
+idade = int(input("Digite sua idade: "))
+email = str(input("Digite seu e-mail: "))
+print('''
+      
+      ''')
+
+print("Nome: %s" %(nome))
+print("Idade:{}".format(idade))
+print("E-mail:", email)
+print(''''
+      
+      
+      Cadastro realizado com sucesso!
+      
+      ''')
+
