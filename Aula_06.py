d52a1bb9c9dec80943123f7bc1559440143f2284
Felipe Dias <15860964+FelipeDiasb@users.noreply.github.com>
2025-03-06 21:56:29 -0300

    Add files via upload

---
+++ Aula_06.py
@@ -0,0 +1,99 @@
+
+import statistics
+
+# palavra = "teste"
+# cumprimento = len( palavra)
+# print(cumprimento)
+
+
+# lista= [10,1,0,6,6,9]
+# lista2=[10,5,12,45,25]
+# numeros = list(range(1,21,2))
+
+# print(numeros)
+
+
+# soma = sum(lista)
+# print('soma', soma)
+
+
+# maior = max(lista)
+# menor = min(lista)
+
+# print('Maior',maior,'menor',menor)
+
+
+# lista2.sort()
+# print(lista2)
+# lista2.sort(reverse=True)
+# print('Organizar',lista2)
+
+# ********************************************************
+
+
+#1* Multiplique 4 por 6 e divida o resultado por 2.
+n1=4
+n2=6
+n3=12
+n4 = 15
+resultado = (n1*n2)
+
+
+print('Multiplique o 4 por 6 e divida:',resultado /2)
+
+#2 Calcule a soma de 5 e 7.
+print('soma de dois números',n1+n2)
+
+#3 Multiplique 4 por 6 e divida o resultado por 2.
+print('Multiplique o 4 por 6 e divida:',resultado /2)
+
+#Eleve 3 à potência de 4.
+print('Potencia',n1**n2 )
+
+#5 Calcule a média de 8, 12 e 15.
+print('Media',(8+12+15)/3 )
+
+
+# Subtraia 10 de 2 e multiplique o resultado por 5.
+resultado2 = 2-10
+print('Subt e Mult', resultado2 * 5  )
+
+
+#Divida 27 por 3 e some 5.
+print('Divisão', (n1/3) + 5 )
+
+
+# Calcule o módulo de 17 por 4.
+modulo = 17
+print('Modulo:', modulo % n1  ) 
+
+# Multiplique 9 por 3 e, em seguida, eleve o resultado ao quadrado.
+print('multiplicação elevada ao quadrado',(9*3)**2 )
+
+#Calcule a raiz quadrada de 81.
+x3 = (81**0.5)
+print('Raiz quadrada de 81',x3)
+
+
+#Adicione 20 a 3 vezes 4.
+print('20 Add: ', (3*4)+20)
+
+#Multiplique 15 por 2 e, em seguida, subtraia 7.
+print('Mul-sub:', (15*2)-7)
+
+#Eleve 5 ao cubo.
+print('Cubo',5**4 )
+
+#Calcule a média de 17, 21 e 25.
+print('Media:',(17+21+25)/3)
+
+#Multiplique 11 por 2 e adicione 7.
+print('7 Add: ', (11*2)+7)
+
+#Subtraia 15 de 3 vezes 8 e divida o resultado por 2.
+sub1 =  (3 * 8) -15
+resul = (sub1)/2
+print(resul)
+
+#Eleve 2 à potência de 10.
+print('Cubo',2**10 )
