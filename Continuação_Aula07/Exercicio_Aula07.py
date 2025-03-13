5aef803784018b2658c85632c26a7d92bd2752dc
Felipe Dias <15860964+FelipeDiasb@users.noreply.github.com>
2025-03-12 21:59:39 -0300

    Add files via upload

---
+++ Continuação_Aula07/Exercicio_Aula07.py
@@ -0,0 +1,71 @@
+
+
+
+
+# loja = {
+#  # inserir os produtos na lista
+#     'fone': 100.00,
+#     'celular':150.00,
+
+# }
+
+
+
+# prod1 = input('Digite o produto:')
+# prod2 = input('Digite o produto:')
+# prod3 = input('Digite o produto:')
+# prod4 = input('Digite o produto:')
+
+
+# list_prod = []
+# list_valores = []
+
+
+# lista_prod.extend([prod1,prod2,prod3,prod4])
+# lista_valores += (loja[prod1],loja[prod2],loja[prod3],loja[prod4])
+
+# soma = sum(list_valores)
+# print('Produtos',list_prod)
+# print('********'*2)
+# print('valores R$:', soma)
+
+
+
+
+# #**************************************
+# # tablet livro fone de ouvido
+
+
+
+# # loja = {
+# # 'fone':100.00,
+# # 'livro x': 50.00,
+# # 'Tablet':5000.00
+# # }
+
+
+
+
+# # prod1 = input('Digite o produto')
+# # prod2 = input('Digite o produto')
+# # prod3 = input('Digite o produto')
+# # prod4 = input('Digite o produto')
+
+
+# # lista_prod = []
+# # lista_valores = []
+
+
+# # lista_prod.extend([prod1,prod2,prod3, prod4])
+# # lista_valores +=(loja[prod1], loja[prod2], loja[prod3], loja[prod4])
+
+
+# # soma = sum(lista_valores)
+# # print('Produtos:', lista_prod)
+# # print('...'*2)
+# # print('R$', soma)
+
+
+
+
+
