f0b8cc1c1fa0f97aa2612ea8317e463565bd9099
Felipe Dias <15860964+FelipeDiasb@users.noreply.github.com>
2025-03-14 21:57:20 -0300

    Add files via upload

---
+++ Aula_09/Exercios.py
@@ -0,0 +1,30 @@
+TESTE = 'python'
+
+
+for n in TESTE:
+    print(n) 
+
+
+dicionario = {
+ 'a':10,
+ 'b':20,
+ 'c':30
+ }
+
+
+k1 = []
+v1 = []
+for k in dicionario.keys():
+    k1.append(k)
+    v1.append(v)
+    print(k)
+
+print(k1)
+
+print(v1)
+
+p = '261515613'
+
+for n in p:
+    f = float(n)
+    print(n)
---
+++ Aula_09/For_hotel.py
@@ -0,0 +1,21 @@
+# testando "For" da aula 09 
+
+
+Escolha_quartos = []
+cliente = []
+precos = []
+
+QUARTOS = ['SIMPLES','DUPLO', 'LUXO']
+PRECOS = [100, 150, 250]
+
+print('''>>>>>>>>>>> Hotel <<<<<<<<<<<<<<<
+
+
+                                       ''')
+
+def calcular_valor(total_dias, quartos):
+    
+    return total_dias * quartos
+
+print(calcular_valor(10,5))
+  
