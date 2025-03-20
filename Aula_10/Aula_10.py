f02853e41a8bff244d64ba8f8a2bfe091b5ab39d
Felipe Dias <15860964+FelipeDiasb@users.noreply.github.com>
2025-03-19 21:58:10 -0300

    Add files via upload

---
+++ Aula_10/Aula_10.py
@@ -0,0 +1,81 @@
+
+# n = 100
+# print(n)
+
+# def teste1 ():
+#  global n
+#  n = 10
+#  print(100*20/n)
+
+
+#  teste1 ()
+
+#  def teste2():
+    
+#     n = 0
+#     print(n)
+    
+#  teste2()
+# print(n)
+
+
+
+
+
+# # i = 1
+# # j = 200
+
+# # def teste ():
+# #     # local
+# #     global i,j
+# #     i = 10      # variaveis blobais 
+# #     j = 20
+# #     print(i,j)    
+
+# # print(i,j)
+# # teste()
+
+
+
+# # # # function
+# # # # Definição para função 
+
+# # # # Função para imprimir um texto na tela
+# # # def display():
+
+# # #     print("Olá, Mundo")
+
+
+
+# # # def cadastrar():
+# # #    nome = input('Nome:')
+# # #    idade = int(input('Idade:'))
+# # #    print(nome, idade)
+
+
+# # # def  calculo_horas():
+# # #     valor_sal = float(input('Valor Salário'))
+# # #     quantidade_horas = float(input('Quantidade Horas'))
+# # #     cal = valor_sal/quantidade_horas
+# # #     print(f'Valor Horas -> {cal:.2f}')
+# # #     extra = cal * 0.5
+# # #     print( 'Horas extra ->',extra)
+# # #     diferenca = extra - cal
+# # #     print(' Valor da extra ->', diferenca)
+# # #     quantidade_horas_realizadas = float(input('Digite a quantidade de extra:'))
+# # #     total = quantidade_horas_realizadas * extra
+# # #     print('Total de hora extra ->',total)
+# # #     total_geral = valor_sal + total
+# # #     print('Salário R$:',total_geral)
+
+
+
+# # # def sistema ():
+# # #    while True:
+# # #       cadastrar()
+# # #       calculo_horas()
+# # #       display()
+  
+
+
+# # # sistema()
---
+++ Aula_10/exercici_10.py
@@ -0,0 +1,34 @@
+
+
+# # 1 CRIE UMA FUNÇÃO PARA COMPARAR 2 NÚMEROS (par ou impar). UTILIZE VARIÁVEIS LOCAIS.
+def impar_par():
+  
+      n1 = int(input('Digite um número: '))
+      n2 = int(input('Digite o segundo número: '))
+
+      
+      if n1 % 2 == 0:
+          print(f'O número {n1} é par.')
+      else:
+          print(f'O número {n1} é ímpar.')
+
+      
+      if n2 % 2 == 0:
+          print(f'O número {n2} é par.')
+      else:
+          print(f'O número {n2} é ímpar.')
+
+impar_par()
+
+continue()
+
+ # CRIE UM AFUNÇÃO PARA MULTIPLICAR 3 NUMEROS.
+
+
+def multipli_3():
+   mp1, mp2 = int(input('Digite um número:')),int(input('Digite um número:'))
+
+
+
+
+multipli_3()
